0b57eea0b5819640c537938149c8382267bc4bcf
Momen <momen.devv@gmail.com>
2026-08-04 01:02:14 +0300

    feat(swagger): integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from '@/infrastructure/logger/logger.service';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -28,6 +29,17 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Multitenant E-commerce API')
+    .setDescription(
+      'API documentation for the Multitenant E-commerce application',
+    )
+    .setVersion('1.0')
+    .addTag('Multitenant E-commerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useLogger(app.get(LoggerService));
   await app.listen(process.env.PORT ?? 3000);
